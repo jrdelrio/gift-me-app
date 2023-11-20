b96264b8851354c3e2ae54e79e7a006b840ed4bf
jrdelrio <jrdelriodom@gmail.com>
2023-11-20 19:43:06 +0100

    ready sidenav, cards of events, cards of gifts

--- src/components/EventSideNavbar.jsx
+++ src/components/EventSideNavbar.jsx
@@ -1,15 +1,31 @@
 import React from "react";
+import "../styles/event-side-navbar.css";
 
 export const EventSideNavbar = (props) => {
 
-    
+  const formattedDate = new Date(props.date).toLocaleDateString("en-US", {
+    month: "short",
+    day: "numeric",
+  });
 
-    return (
-        <li>
-            <a href="#" className="block p-2 hover:bg-gray-700 flex items-center">
-              {/* <i className="fas fa-chevron-right mr-2 text-xs"></i> */}
-              Firmas pendientes
-            </a>
-          </li>
-    )
+  return (
+    <li key={props.key} style={{ margin: "3px" }}>
+      <div className="card text-center">
+        <div className="card-header event-title py-1">
+          {props.title}
+          {props.myEvent ? <i className="fa-solid fa-pen-to-square" style={{float: "right"}}></i> : ""}
+        </div>
+        <div className="card-footer text-body-secondary flex mx-0 p-0 items-center">
+          <div className="card-footer event-footer-left px-0 flex-grow-0 border-end px-2">
+            <i class="fa-solid fa-calendar-days"></i>  {formattedDate}
+          </div>
+
+          <div className="card-footer event-footer-right px-0 flex-grow">
+            {props.itemsReady} of {props.numItems} items
+          </div>
+        </div>
+      </div>
+    </li>
+
+  )
 }
