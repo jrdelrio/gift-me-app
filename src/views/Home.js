3ba05f496848b6b6c362bc7cff0da5112937fcb0
jrdelrio <jrdelriodom@gmail.com>
2023-11-30 14:54:06 +0100

    fetching giftas from backend. I still need to add SQLAlchemy

--- src/views/Home.js
+++ src/views/Home.js
@@ -6,10 +6,29 @@ import { HomeHeader } from "../components/HomeHeader.jsx";
 import { GiftHomeCard } from "../components/GiftHomeCard.jsx";
 import giftIcon from "../images/gift-icon.jpg";
 import { MainFooter } from "../components/MainFooter.jsx";
-import giftsList from "../jsons/main-gifts-list.json"
+import giftsList from "../jsons/main-gifts-list.json";
 
 export const Home = () => {
-  const [mainGiftsList, setMainGiftsList] = useState(giftsList);
+  const [mainGiftsList, setMainGiftsList] = useState([]);
+
+  useEffect(() => {
+    const fetchGiftList = async () => {
+      try {
+        const response = await fetch('http://127.0.0.1:5000/USER/giftList/');
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        const data = await response.json();
+        setMainGiftsList(data)
+        console.log(data)
+      } catch (error) {
+        console.error("Fetching error:", error);
+        // Handle the error appropriately in your application
+      }
+    };
+
+    fetchGiftList();
+  }, []);
 
   return (
     <div className="App">
