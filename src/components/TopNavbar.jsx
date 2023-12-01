a481f27da803379d861125b2723f8f950bb711e7
jrdelrio <jrdelriodom@gmail.com>
2023-12-01 16:56:49 +0100

    upgrades. Link for logout, login view and cors

--- src/components/TopNavbar.jsx
+++ src/components/TopNavbar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import '../styles/header-navbar.css';
 import portrait from "../images/portrait.jpg"
@@ -19,11 +20,8 @@ export const TopNavbar = () => {
             🎁 Gift <span>me</span> !
           </h1>
         </div>
-        <div>
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-            Login/Logout
-          </button>
-        </div>
+          <Link className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" to="../login">Login/Logout</Link>
+
         <div className="flex items-center space-x-4">
           <span>Welcome, {username}</span>
           {/* {!profilePicture ? <i className="fas fa-user-circle text-2xl"></i> : <img src={profilePicture} />} */}
